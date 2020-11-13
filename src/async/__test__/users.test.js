ffbc631fba3e01d5bf71258c617d3697cf53682f
yuan qihang <qihang.yuan@thoughtworks.com>
2020-11-13 19:58:19 +0800

    complete async folder

--- src/async/__test__/users.test.js
+++ src/async/__test__/users.test.js
@@ -1,5 +1,8 @@
+import getUsers from "../users";
+
 describe("users", () => {
-  test("should get users data with mock axios get", () => {
-    // TODO 13: add async test with manual mock
+  test("should get users data with mock axios get", async () => {
+    // 13: add async test with manual mock
+    await expect(getUsers()).rejects.toThrow("Network Error");
   });
 });
